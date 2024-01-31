e48b7ed021f6d524da5b9b5808bd9e916d8121f3
quatarnary <contact@ilyasbugra.com>
2024-01-31 12:55:11 +0300

    Initial Commit

---
+++ challenges-100-days-of-swift-ui/challenges_100_days_of_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenges_100_days_of_swift_uiApp.swift
+//  challenges-100-days-of-swift-ui
+//
+//  Created by Bugra Aslan on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct challenges_100_days_of_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
