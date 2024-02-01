21976421458f3bf30e27a3604faef9d2c317ca7a
quatarnary <contact@ilyasbugra.com>
2024-02-01 14:52:12 +0300

    Default View for checkpoint UIs

--- challenges-100-days-of-swift-ui/DefaultUIView.swift
+++ challenges-100-days-of-swift-ui/DefaultUIView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct DefaultUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("") {
+                    Text("")
+                }
+                Section("") {
+                    Text("")
+                }
+                Section("") {
+                    Text("")
+                }
+            }
+            .navigationTitle("Checkpoint Name")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
